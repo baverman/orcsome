85fed30123eb39487ca6accc6bf86c384051e0aa
Anton Bobrov <bobrov@vl.ru>
2014-04-19 19:53:44 +0400

    include cffi source in distribution

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
-from orcsome import VERSION
 from setuptools.command import easy_install
+
+import orcsome
+import orcsome.xlib
 
 def install_script(self, dist, script_name, script_text, dev_path=None):
     script_text = easy_install.get_script_header(script_text) + (
@@ -12,13 +14,14 @@ easy_install.easy_install.install_script = install_script
 
 setup(
     name     = 'orcsome',
-    version  = VERSION,
+    version  = orcsome.VERSION,
     author   = 'Anton Bobrov',
     author_email = 'bobrov@vl.ru',
     description = 'Scripting extension for NETWM compliant window managers',
     long_description = open('README.rst').read(),
     zip_safe   = False,
     packages = find_packages(exclude=('tests', )),
+    ext_modules=[orcsome.xlib.ffi.verifier.get_extension()],
     install_requires = ['cffi'],
     include_package_data = True,
     scripts = ['bin/orcsome'],
